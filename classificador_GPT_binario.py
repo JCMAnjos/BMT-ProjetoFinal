f865a6f0c7f919563beb3918b1e4d8d2ed578e4b
Ian Agra <45951701+ianagra@users.noreply.github.com>
2023-06-24 14:14:57 -0300

    Adicionados arquivos
    Adicionados classificadores BERT e GPT

---
+++ classificador_GPT_binario.py
@@ -0,0 +1,32 @@
+import openai
+import pandas as pd
+
+openai.api_key = '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'
+
+#Função para classificar o comentário
+def classificar_comentario(comentario):
+    resposta = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Classifique o seguinte comentário sobre inteligência artificial como 'p' (positivo) ou 'n' (negativo):\n\n{comentario}\n\nClassificação:",
+        temperature=0.0,
+        max_tokens=1,
+        n=1,
+        stop=None,
+        timeout=15
+    )
+    classificacao = resposta.choices[0].text.strip().lower()
+    return classificacao
+
+#Ler o arquivo CSV
+df = pd.read_csv('comentarios.csv', sep=';', index_col=0, header=0)
+
+#Remover os comentários classificados manualmente como neutros
+df = df[df['sentimento_humano'] != 'x'].copy()
+
+#Adicionar uma nova coluna ao DataFrame com a classificação feita
+df['sentimento_GPT'] = df['comentario'].apply(classificar_comentario)
+
+#Salvar o DataFrame com a coluna de classificação em um novo arquivo CSV
+df.to_csv('comentarios_GPT_binario.csv')
+
+print("A classificação dos comentários foi concluída.")
